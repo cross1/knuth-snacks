467fc31e5d7096905aeb721e85cf14e86c57fb53
cross1 <crossi@g.hmc.edu>
2017-06-15 10:50:07 -0700

    added requests page

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/requests')
+def requests():
+    return render_template('requests.html')
+
+@app.route('/shopping-list')
+def requests():
+    return render_template('shopping-list.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
