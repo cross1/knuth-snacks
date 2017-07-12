e4bf94be723db246952354c87cbf206ca4e00229
cross1 <crossi@g.hmc.edu>
2017-07-12 13:54:00 -0700

    I'm trying to fix this

--- moresnacks.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('#button').click(function(){
-    var newItem = $('input[name=snack-suggestion]').val();
-    $('.list-of-snacks').append('<div class="newItem">' + newItem +'</div>');
-  });
-});
