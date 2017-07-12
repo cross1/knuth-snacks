49297cbf47c73229d6186282ca50141831cc0d5e
cross1 <crossi@g.hmc.edu>
2017-07-12 13:18:04 -0700

    fixed flask, added location form

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from app.db_config import db_uri
 
 app = Flask(__name__)
+app.config.from_object('app.config')
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 #db_uri is stored in db_config.py
 
--- app/models.py
+++ app/models.py
@@ -3,7 +3,13 @@ from app import db
 class Snacks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     snackname = db.Column(db.String(64))
+    out = db.Column(db.Boolean)
+    location = db.Column(db.String(64))
+    bumps = db.Column(db.Integer)
 
 
     def __init__(self, name):
         self.snackname = name
+        self.bumps = 0
+        self.out = True
+        self.location = "Other"
